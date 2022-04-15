c8a8c53229f674820850d6bb7991cbd743087b0d
champlinyanis <champlinyanis@gmail.com>
2022-04-15 10:59:03 +0200

    more tests

--- Module_test.py
+++ Module_test.py
@@ -1,7 +1,29 @@
-from Module import Person, Wizard
+from Module import Person, Wizard, HealthPotion
 
 person = Person('Batman')
 wizard = Wizard(Person('Gandalf'))
+
+def test_potion_person():
+    expected_result = True
+    hp_before_potion = person.get_life_points  
+    HealthPotion.was_used_by(person)
+    hp_after_potion = person.get_life_points
+    if hp_before_potion < hp_before_potion:
+        actual_result = True
+    else:
+        actual_result = False
+    assert expected_result == actual_result
+    
+def test_potion_wizard():
+    expected_result = True
+    hp_before_potion = wizard.get_life_points  
+    HealthPotion.was_used_by(wizard)
+    hp_after_potion = wizard.get_life_points
+    if hp_before_potion < hp_before_potion:
+        actual_result = True
+    else:
+        actual_result = False
+    assert expected_result == actual_result    
 
 def test_person_suicide():
     expected_result = True
@@ -18,7 +40,7 @@ def test_wizard_suicide():
     assert expected_result == actual_result
 
 def test_death_duel_person_first_no_heal():
-    expected_result = 'Wizard' 
+    expected_result = 'Gandalf' 
     while person.life_points > 0 & wizard.life_points > 0: 
         person.hit(wizard)
         wizard.hit(person)        
@@ -29,7 +51,7 @@ def test_death_duel_person_first_no_heal():
     assert expected_result == actual_result
     
 def test_death_duel_wizard_first_no_heal():
-    expected_result = 'Wizard'
+    expected_result = 'Gandalf'
     while person.life_points > 0 & wizard.life_points > 0:
         wizard.hit(person)
         person.hit(wizard)    
