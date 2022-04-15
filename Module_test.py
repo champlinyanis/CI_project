287ee4320a1131e9b6176decf4a4f4b568b078e2
champlinyanis <champlinyanis@gmail.com>
2022-04-15 12:46:53 +0200

    mucho testo

--- Module_test.py
+++ Module_test.py
@@ -1,6 +1,7 @@
 from Module import Person, Wizard, HealthPotion
 from main import Duel
 
+# potion
 def test_potion_person():
     person = Person('Batman')
     expected_result = True
@@ -25,6 +26,7 @@ def test_potion_wizard():
         actual_result = False
     assert expected_result == actual_result    
 
+# suicide
 def test_suicide_person():
     person = Person('Batman')
     expected_result = True
@@ -41,6 +43,7 @@ def test_suicide_wizard():
     actual_result = wizard.is_dead()
     assert expected_result == actual_result
 
+# duel no heal
 def test_death_duel_no_heal_person():
     person = Person('Batman')
     wizard = Wizard('Gandalf')
@@ -66,7 +69,8 @@ def test_death_duel_no_heal_wizard():
     else:
         actual_result = wizard.name
     assert expected_result == actual_result
-    
+ 
+# duel - heal   
 def test_death_duel_heal_person():
     person = Person('Batman')
     wizard = Wizard('Gandalf')
@@ -75,14 +79,14 @@ def test_death_duel_heal_person():
         person.hit(wizard)
         wizard.hit(person)
         HealthPotion.was_used_by(person)
-        print("person HP: ", person.get_life_points())
-        print("wizard HP: ", wizard.get_life_points())
+        # print("person HP: ", person.get_life_points())
+        # print("wizard HP: ", wizard.get_life_points())
     if(person.get_life_points() > wizard.get_life_points()):
         actual_result = person.name
     else:
         actual_result = wizard.name
     assert expected_result == actual_result
-    
+ 
 def test_death_duel_heal_wizard():
     person = Person('Batman')
     wizard = Wizard('Gandalf')
@@ -91,8 +95,8 @@ def test_death_duel_heal_wizard():
         person.hit(wizard)
         wizard.hit(person)
         HealthPotion.was_used_by(wizard)
-        print("person HP: ", person.get_life_points())
-        print("wizard HP: ", wizard.get_life_points())
+        # print("person HP: ", person.get_life_points())
+        # print("wizard HP: ", wizard.get_life_points())
     if(person.get_life_points() > wizard.get_life_points()):
         actual_result = person.name
     else:
@@ -106,12 +110,77 @@ def test_death_duel_heal():
     while person.get_life_points() > 0 and wizard.get_life_points() > 0:
         person.hit(wizard)
         wizard.hit(person)
-        HealthPotion.was_used_by(wizard)
-        HealthPotion.was_used_by(person)
-        print("person HP: ", person.get_life_points())
-        print("wizard HP: ", wizard.get_life_points())
+        # HealthPotion.was_used_by(wizard)
+        # HealthPotion.was_used_by(person)
     if(person.get_life_points() > wizard.get_life_points()):
         actual_result = person.name
     else:
         actual_result = wizard.name
+    assert expected_result == actual_result
+  
+# negative life gain  
+def test_death_person():    
+    person = Person('Batman')
+    expected_result = True
+    person.gained_life_points(-person.get_life_points())
+    if(person.is_dead):
+        actual_result = True
+    else:
+        actual_result = False
+    assert actual_result == expected_result
+    
+def test_death_wizard():    
+    wizard = Wizard('Gandalf')
+    expected_result = True
+    wizard.gained_life_points(-wizard.get_life_points())
+    if(wizard.is_dead):
+        actual_result = True
+    else:
+        actual_result = False
+    assert actual_result == expected_result
+  
+# life gain  
+def test_life_gain_person():
+    person = Person('Batman')
+    expected_result = True
+    hp_before_potion = person.get_life_points()
+    person.gained_life_points(1)
+    hp_after_potion = person.get_life_points()
+    if hp_before_potion < hp_after_potion:
+        actual_result = True
+    else:
+        actual_result = False
+    assert expected_result == actual_result
+    
+def test_life_gain_wizard():
+    wizard = Wizard('Gandalf')
+    expected_result = True
+    hp_before_potion = wizard.get_life_points()
+    wizard.gained_life_points(1)
+    hp_after_potion = wizard.get_life_points()
+    if hp_before_potion < hp_after_potion:
+        actual_result = True
+    else:
+        actual_result = False
+    assert expected_result == actual_result 
+    
+# potion + life gain
+def test_potion_plus_life_gain_person():
+    person = Person('Batman')
+    expected_result = 11
+    hp_before_potion_and_life_gain = person.get_life_points()
+    HealthPotion.was_used_by(person)
+    person.gained_life_points(1)
+    hp_after_potion_and_life_gain = person.get_life_points()
+    actual_result = hp_after_potion_and_life_gain - hp_before_potion_and_life_gain
+    assert expected_result == actual_result
+    
+def test_potion_plus_life_gain_wizard():
+    wizard = Wizard('Gandalf')
+    expected_result = 11
+    hp_before_potion_and_life_gain = wizard.get_life_points()
+    HealthPotion.was_used_by(wizard)
+    wizard.gained_life_points(1)
+    hp_after_potion_and_life_gain = wizard.get_life_points()
+    actual_result = hp_after_potion_and_life_gain - hp_before_potion_and_life_gain
     assert expected_result == actual_result
--- main.py
+++ main.py
@@ -5,26 +5,71 @@ user_1 = Person("Hero")
 user_2 = Wizard("Wizard")
 
 def Duel():
+    
+    hits = []
+    potions = []
+    turn = 1
+    
+    print("The duel begins")
+    
+    print("Hero's HP : " + str(user_1.get_life_points()))
+    print("Wizard's HP : " + str(user_2.get_life_points()))
+    
+    print("-----------------------")
+    
     while user_1.is_dead() == False and user_2.is_dead() == False:
-        random = np.random.randint(2)
+        
+        print("* Round " + str(turn) + " *")
+        
+        random = np.random.randint(2)        
         
         if random == 1:
             user_1.hit(user_2)
+            hits.append(user_1.name)
+            print("> Hero hits Wizard")
+            print("Wizard's HP after the hit : " + str(user_2.get_life_points()))
         else:
             user_2.hit(user_1)
+            hits.append(user_2.name)
+            print("> Wizard hits Hero")
+            print("Hero's HP after the hit : " + str(user_1.get_life_points()))
 
         random_health_potion_use = np.random.randint(3)
-
+        
         if random_health_potion_use == 0:
             HealthPotion.was_used_by(user_1)
+            potions.append(user_1.name)
+            print("> Hero uses a potion")
+            print("Hero's HP after the potion : " + str(user_1.get_life_points()))
         elif random_health_potion_use == 1: # heal wizard
             HealthPotion.was_used_by(user_2)
+            potions.append(user_2.name)
+            print("> Wizard uses a potion")
+            print("Wizard's HP after the potion : " + str(user_2.get_life_points()))
         else:
+            print("> No potion used")
+            potions.append("no pot")
             pass
-
-    if user_1.get_life_points() <= 0:
-        print(user_1.name + " wins")
-    if user_2.get_life_points() <= 0:
-        print(user_2.name + " wins")
+        turn += 1
         
-# Duel()
+        print("Hero's HP at the of turn " + str(turn) + " : " + str(user_1.get_life_points()))
+        print("Wizard's HP at the end of turn " + str(turn) + " : " + str(user_2.get_life_points()))
+        
+        print("-----------------------")
+        
+    if(user_1.get_life_points() > user_2.get_life_points()):
+        print(user_1.name + " wins")
+    elif(user_1.get_life_points() < user_2.get_life_points()):
+        print(user_2.name + " wins")
+    else:
+        print("draw")
+        
+    print("Total hits Hero : " + str(hits.count("Hero")))
+    print("Total hits Wizard : " + str(hits.count("Wizard")))
+    print("Total potions Hero : " + str(potions.count("Hero")))
+    print("Total potions Wizard : " + str(potions.count("Wizard")))
+    # print("potions : " + str(potions))
+    # print("nb of turns : " + str(turn))
+    
+            
+Duel()
